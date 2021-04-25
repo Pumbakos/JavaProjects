65f9f20198275f96d3a6c80c847c8fa7408dfff5
Dawid Pałubiak <dawid.palubiak@gmail.com>
2021-04-25 22:36:15 +0200

    Add multithreading @play(), add listSongs()

--- AudioPlayer/src/audioPlayer/AudioPlayer.java
+++ AudioPlayer/src/audioPlayer/AudioPlayer.java
@@ -10,13 +10,17 @@ import java.util.List;
 import java.util.Scanner;
 
 public class AudioPlayer {
-    private List<File> musicList = new ArrayList();
     private final String MUSIC_FOLDER = "D:\\Desktop\\CODE\\JAVA\\AudioPlayer\\music\\";
     private final Scanner scanner = new Scanner(System.in);
+    private AdvancedPlayer player;
+    private Thread backgroundPlay;
+    private List<File> musicList = new ArrayList();
     private String currentSong;
 
-
-    public void openFolder(){
+    /**
+     * Opens default music folder - my local actually :)
+     */
+    public void openFolder() {
         File path = new File(MUSIC_FOLDER);
         System.out.println(path);
         for (File file : path.listFiles()) {
@@ -29,47 +33,89 @@ public class AudioPlayer {
         }
     }
 
-    public String chooseSong(){
-        int song;
-        String pathToSong;
+    /**
+     * Displays the songs in the current folder and waits for the user's keyboard input
+     * @deprecated return full path to song
+     */
+    public void chooseSong() {
+        int usersSongChoice;
+//        String pathToSong;
+        listSongs();
 
-        for (int i = 0; i < musicList.size(); i++) {
-            System.out.println((i+1) + ". " + musicList.get(i));
-        }
+        System.out.print("\nChoose song(1-" + musicList.size() + "): ");
+        usersSongChoice = scanner.nextInt();
+        currentSong = musicList.get(usersSongChoice - 1).getName();
 
-        System.out.print("\nChoose song(1-"+ musicList.size() +"): ");
-        song = scanner.nextInt();
-        currentSong = musicList.get(song -1).getName();
-
-        pathToSong = this.MUSIC_FOLDER + currentSong;
-
-        return pathToSong;
+//        pathToSong = this.MUSIC_FOLDER + currentSong;
+//        return pathToSong;
     }
 
-    public void play(String path){
-        try {
-            BufferedInputStream buffer = new BufferedInputStream(
-                    new FileInputStream(path));
-            AdvancedPlayer player = new AdvancedPlayer(buffer);
-            player.play();
+    /**
+     * Displays songs saved at @see musicList
+     */
+    private void listSongs() {
+        for (int i = 0; i < musicList.size(); i++) {
+            System.out.println((i + 1) + ". " + musicList.get(i).getName().substring(0, musicList.get(i).getName().length() -4).replace('_',' '));
         }
-        catch (Exception e) {
+    }
+
+    /**
+     * Prepares app for background activity
+     * Waits until the start of the thread
+     * @param path : full path to song
+     */
+    private void prepareSong(String path){
+        backgroundPlay = new Thread(() -> {
+            try {
+                BufferedInputStream buffer = new BufferedInputStream(
+                        new FileInputStream(path));
+                player = new AdvancedPlayer(buffer);
+                player.play();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+
+    public synchronized void play() {
+        prepareSong(MUSIC_FOLDER+currentSong);
+        try {
+            backgroundPlay.start();
+        }catch (IllegalThreadStateException e){
             e.printStackTrace();
         }
     }
 
-    public void pause(){}
+    public void pause() {
+    }
 
-    public void resume(){}
+    public void resume() {
+    }
 
-    public void stop(){}
+    /**
+     * stops background current playing song
+     */
+    public void stop() {
+        try {
+            backgroundPlay.interrupt();
+        }catch(SecurityException e){
+            e.printStackTrace();
+        }
+    }
 
-    public void next(){}
+    public void next() {
+    }
 
-    public void previous(){}
+    public void previous() {
+    }
 
-    private void listFilesForFolder(File folder) {
-        for (File file : folder.listFiles()) {
+    /**
+     * lists all files within given folder - excludes sub-folders
+     * @param path: full path to folder where stored songs
+     */
+    private void listFilesForFolder(File path) {
+        for (File file : path.listFiles()) {
             if (file.isDirectory()) {
 //                listFilesForFolder(file);
                 continue;
@@ -79,9 +125,14 @@ public class AudioPlayer {
         }
     }
 
-    public void listFiles(){
-        for (File file : this.musicList) {
-            System.out.println(file);
-        }
+    public String getCurrentSong(){
+        return currentSong;
+    }
+
+    /**
+     * @return full path to chosen song
+     */
+    private String getPathToSong(){
+        return MUSIC_FOLDER.concat(currentSong);
     }
 }
--- AudioPlayer/src/audioPlayer/Main.java
+++ AudioPlayer/src/audioPlayer/Main.java
@@ -3,14 +3,11 @@ package audioPlayer;
 public class Main {
     public static void main(String[] args) {
         AudioPlayer player = new AudioPlayer();
-//        String bandyta = "D:\\Desktop\\CODE\\JAVA\\AudioPlayer\\src\\audioPlayer\\music\\bandyta.mp3";
 
         player.openFolder();
-        player.listFiles();
 
-        String song = player.chooseSong();
+        player.chooseSong();
 
-        player.play(song);
-//        System.out.println(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath() + "music/");
+//        player.play();
     }
 }
